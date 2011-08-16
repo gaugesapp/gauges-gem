8ea7fa0d08bee138e2803c941862c232e6fee1bd
John Nunemaker <nunemaker@gmail.com>
2011-08-16 19:16:49 -0400

    Added content examples.

---
+++ examples/content.rb
@@ -0,0 +1,19 @@
+$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
+require 'pp'
+require 'rubygems'
+require 'gauges'
+
+id = '4d5f4992089bb618a2000005'
+ga = Gauges.new(:email => 'john@orderedlist.com', :password => 'foobar')
+
+response = ga.content(id)
+pp response['content'].size
+
+response = ga.content(id, :page => 2)
+pp response['content'].size
+
+response = ga.content(id, :page => 3)
+pp response['content'].size
+
+response = ga.content(id, :date => '2011-08-01')
+pp response['content'].size
